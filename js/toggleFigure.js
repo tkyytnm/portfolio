69d0db9876af3806a61c330ab77db0e7add00749
Takuyay <mechanicalgiraffe@gmail.com>
2021-05-27 11:14:50 +0900

    Separate scripts for concern

--- js/toggleFigure.js
+++ js/toggleFigure.js
@@ -1,22 +1,3 @@
-// Switch Dark Mode
-const darkButton = document.getElementById('dark');
-
-const switchDark = function() {
-  const body = document.getElementsByTagName('body')[0];
-  if (!body.className) {
-    body.className = 'dark';
-    darkButton.querySelector('i').classList.remove('fa-toggle-off');
-    darkButton.querySelector('i').classList.add('fa-toggle-on');
-  } else {
-    body.className = '';
-    darkButton.querySelector('i').classList.add('fa-toggle-off');
-    darkButton.querySelector('i').classList.remove('fa-toggle-on');
-  }
-}
-
-darkButton.addEventListener('click', switchDark);
-
-
 // Projects Toggle
 
 const project1 = document.getElementById("company-home-page-with-flexbox");
