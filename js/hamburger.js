e541fa808ef1a80c84e111d8fafe3328e5602f44
Takuyay <mechanicalgiraffe@gmail.com>
2021-05-27 14:53:47 +0900

    Make hamburger menu close as link clicked

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,18 +1,29 @@
-const ham = document.getElementById('hamburger');
-const nav = ham.parentElement.getElementsByTagName('nav')[0];
-const aLink = ham.parentElement.getElementsByTagName('a');
+const ham = document.getElementById("hamburger");
+const nav = ham.parentElement.getElementsByTagName("nav")[0];
+const aLink = ham.parentElement.getElementsByTagName("a");
 
 console.log(aLink);
 
-const toggleHam = function() {
-  if (!ham.className) {
-    ham.className = 'on';
-    nav.className = 'on';
-  } else {
-    ham.className = '';
-    nav.className = '';
-  }
-  
-}
+const openHam = function () {
+  ham.className = "on";
+  nav.className = "on";
+};
 
-ham.addEventListener('click', toggleHam);
+const closeHam = function () {
+  ham.className = "";
+  nav.className = "";
+};
+
+const toggleHam = function () {
+  if (!ham.className) {
+    openHam();
+  } else {
+    closeHam();
+  }
+};
+
+ham.addEventListener("click", toggleHam);
+
+for (let i=0; i<aLink.length; i++) {
+  aLink[i].addEventListener('click', closeHam);
+}
